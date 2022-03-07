c3986a47cf645ea85cd23f25130ecd205bb5472c
jaywcjlove <398188662@qq.com>
2022-03-07 16:24:55 +0800

    init project.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ncc
-Simple CLI for compiling a Node.js module into a single file, together with all its dependencies, gcc-style.
+core/README.md
---
+++ examples/bundle/.kktrc.js
@@ -0,0 +1,23 @@
+import lessModules from '@kkt/less-modules';
+
+export default (conf, env, options) => {
+  conf = lessModules(conf, env, options);
+  if (options.bundle) {
+    conf.externals = {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    };
+  }
+  // console.log('conf:', env, conf);
+  return conf;
+};
