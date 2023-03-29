52acf12355635417af899c61a3ef099ec5077e64
jaywcjlove <398188662@qq.com>
2023-03-29 23:44:22 +0800

    chore(deps): update dependency tsbb to v4 #14
    https://github.com/jaywcjlove/tsbb/issues/439

--- examples/bundle/src/index.tsx
+++ examples/bundle/src/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './app/App';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
